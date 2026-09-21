8193ba4239d45b02a891e77137d8b51ffb751142
wobushi041 <wobushi041@gmail.com>
2026-09-21 13:21:08 +0800

    refactor(api): 清理 axios 请求/响应拦截器的调试日志

--- matchsystem-frontend/src/plugins/myAxios.ts
+++ matchsystem-frontend/src/plugins/myAxios.ts
@@ -10,7 +10,6 @@ myAxios.defaults.withCredentials = true; // 配置为true
 
 // Add a request interceptor
 myAxios.interceptors.request.use(function (config) {
-    console.log('我要发请求啦', config)
     // Do something before request is sent
     return config;
 }, function (error) {
@@ -20,7 +19,6 @@ myAxios.interceptors.request.use(function (config) {
 
 // Add a response interceptor
 myAxios.interceptors.response.use(function (response) {
-    console.log('我收到你的响应啦', response)
     // 未登录则跳转到登录页
     if (response?.data?.code === 40100) {
         const redirectUrl = window.location.href;
